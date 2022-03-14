72c1126ad506a6296bcf9fc4a74e448ed12d5488
educia27 <edd1114@gmail.com>
2022-03-14 18:06:43 -0500

    query change

--- queries.sql
+++ queries.sql
@@ -12,7 +12,7 @@ DROP TABLE job;
 
 -- Part 4: Test it with SQL
 
-SELECT name, description
+SELECT *
 FROM skill
 INNER JOIN job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_id IS NOT NULL
